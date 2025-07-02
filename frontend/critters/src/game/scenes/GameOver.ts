128268eb7758f35859eab299a5dd0b5b645dafb0
xintian.wu <xin-tian.wu@allianz.de>
2025-07-02 21:11:19 +0200

    making the game (phaserJS)

--- frontend/critters/src/game/scenes/GameOver.ts
+++ frontend/critters/src/game/scenes/GameOver.ts
@@ -16,13 +16,13 @@ export class GameOver extends Scene {
   create() {
     this.add.image(512, 384, 'background');
 
-    this.add.text(512, 384, 'Your Pet has Died', {
+    this.add.text(512, 384, 'Game Over', {
       font: '48px Arial',
       color: '#ff0000',
     }).setOrigin(0.5);
 
     this.input.once('pointerdown', () => {
-      this.scene.start('PetMenu', { eventBus: this.eventBus });
+      this.scene.start('MainMenu', { eventBus: this.eventBus });
     });
   }
 }
