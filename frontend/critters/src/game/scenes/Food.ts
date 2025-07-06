7155af55f6748d9871e968d95d06416e815cdb3c
xintian.wu <xin-tian.wu@allianz.de>
2025-07-07 01:02:05 +0200

    making food scene (first attempt)

--- frontend/critters/src/game/scenes/Food.ts
+++ frontend/critters/src/game/scenes/Food.ts
@@ -1,5 +1,19 @@
 import {Scene} from 'phaser';
 
-export class FoodScene extends Scene{
+export class FoodPantry extends Scene{
+  bg!: Phaser.GameObjects.Image;
 
+  constructor() {
+    super('FoodPantry');
+  }
+
+  create() {
+    const { width, height} = this.scale;
+
+    // Add background first
+    this.bg = this.add.image(0, 0, 'background').setOrigin(0,0);
+    this.bg.setDisplaySize(width, height);
+
+
+  }
 }
