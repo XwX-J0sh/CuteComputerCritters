977a0f259e73f40aaa896cd391aee9daa54365ba
xintian.wu <xin-tian.wu@allianz.de>
2025-07-08 19:11:12 +0200

    fix slight delay in critter response sound, add sound when stats too low, gameover screen

--- backend/src/main/java/com/CuteComputerCritters/backend/api/scheduler/CritterScheduler.java
+++ backend/src/main/java/com/CuteComputerCritters/backend/api/scheduler/CritterScheduler.java
@@ -77,13 +77,6 @@ public class CritterScheduler {
             freshCritter.setHealthy(false);
         }
 
-        //call after one minute DEBUG
-        if (evolutionStage == 1 && freshCritter.getTotalActiveTime() >= 60000) {
-            critter.setHasCalled(true);
-            critter.setCalledSince(now);
-            log.info("Critter {} has called! (DEBUG)", critter.getCritterId());
-        }
-
         // Evolution
         CritterEvolutions currentEvolution = freshCritter.getEvolutionStage();
 
