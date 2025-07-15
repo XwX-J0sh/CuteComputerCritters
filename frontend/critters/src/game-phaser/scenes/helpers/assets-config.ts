a35e1788e4d0b6f42ffb7365119708ee63a6cf2c
xintian.wu <xin-tian.wu@allianz.de>
2025-07-15 18:44:38 +0200

    animation loader

---
+++ frontend/critters/src/game-phaser/scenes/helpers/assets-config.ts
@@ -0,0 +1,57 @@
+export const ASSET_CONFIG = {
+    // Common assets
+    audio: {
+        critterCall: '../assets/sounds/tamagotchi_alert.mp3'
+    },
+
+    // Baby sprites
+    baby: {
+        idle: '../assets/baby/baby_idle1.PNG',
+        eat: '../assets/baby/baby_eating.PNG',
+        sick_idle: '../assets/baby/baby_sick_idle.PNG',
+        turn_sick: '../assets/baby/baby_turn_sick.PNG'
+    },
+
+    // Adult variants
+    variants: {
+        chiikawa: {
+            idle: '../assets/chiikawa/chiikawa_idle1.PNG',
+            eat: '../assets/chiikawa/chiikawa_eating.PNG',
+            sick_idle: '../assets/chiikawa/chiikawa_sick_idle.PNG',
+            turn_sick: '../assets/chiikawa/chiikawa_turn_sick.PNG'
+        },
+        hachiware: {
+            idle: '../assets/hachiware/hachiware_idle1.PNG',
+            eat: '../assets/hachiware/hachiware_eating.PNG',
+            sick_idle: '../assets/hachiware/hachiware_sick_idle.PNG',
+            turn_sick: '../assets/hachiware/hachiware_turn_sick.PNG'
+        },
+        usagi: {
+            idle: '../assets/usagi/usagi_idle1.PNG',
+            eat: '../assets/usagi/usagi_eating.PNG',
+            sick_idle: '../assets/usagi/usagi_sick_idle.PNG',
+            turn_sick: '../assets/usagi/usagi_turn_sick.PNG'
+        },
+        momonga: {
+            idle: '../assets/momonga/momonga_idle1.PNG',
+            eat: '../assets/momonga/momonga_eating.PNG',
+            sick_idle: '../assets/momonga/momonga_sick_idle.PNG',
+            turn_sick: '../assets/momonga/momonga_turn_sick.PNG'
+        },
+        shisa: {
+            idle: '../assets/shisa/shisa_idle1.PNG',
+            eat: '../assets/shisa/shisa_eating.PNG',
+            sick_idle: '../assets/shisa/shisa_sick_idle.PNG',
+            turn_sick: '../assets/shisa/shisa_turn_sick.PNG'
+        },
+
+    },
+
+    // Common settings
+    spritesheetSettings: {
+        frameWidth: 256,
+        frameHeight: 256,
+        margin: 0,
+        spacing: 0
+    }
+};
